fb867b9ab8c980242ba22864e11e729a5145f873
unknown <joseph.yzp@gmail.com>
2022-11-12 10:50:31 +0800

    prime number

---
+++ 1068.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, t, n = 0;
+	cin >> a;
+	cin >> b;
+
+	for (int i = 0; i < a; i++) {
+		cin >> t;
+		if (t == b)
+			n = n + 1;
+
+	}
+	cout << n;
+}
---
+++ prime number.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main () {
+	int a, flag = 0;
+	cin >> a;
+	for (int i = 2; i < sqrt(a); i++) {
+		if (a % i == 0)
+			flag = 1;
+
+	}
+	if (flag == 1)
+		cout << "not prime number";
+	else
+		cout << "is prime number";
+}
