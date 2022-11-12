0ec3d30946d077d2512478809200088ddd51f6c3
unknown <joseph.yzp@gmail.com>
2022-11-12 09:52:16 +0800

    value of span

---
+++ Untitled1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n, a, b, max = 0, min;
+	cin >> n;
+	cin >> a;
+	min = a;
+	max = a;
+	for (int i = 1; i < n; i++) {
+		cin >> b;
+		if (b > max)
+			max = b;
+		if (b < max)
+			min = b;
+	}
+	cout << max - min;
+
+}
