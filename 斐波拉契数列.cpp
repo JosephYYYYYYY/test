b14b8a61840c37fd21d94a0aa832d3804886440e
unknown <joseph.yzp@gmail.com>
2022-11-12 11:20:46 +0800

    fibonacci sequence

---
+++ 斐波拉契数列.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main () {
+	int a = 0, b = 1, c, n;
+	cin >> n;
+	printf("%d %d", a, b);
+	for (int i = 2; i < n; ++i) {
+		c = a + b;
+		printf("%d", c);
+		a = b, b = c;
+	}
+	return 0;
+
+}
